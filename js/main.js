7946bd7c6e78903f1faf2823571fe94faf8e6c36
Alexandra Lillo <stereodreaming@gmail.com>
2021-09-02 17:15:15 +0200

    added const and function

---
+++ js/main.js
@@ -0,0 +1,24 @@
+'use strict';
+
+//CONSTANTES//
+
+const inputNumber = document.querySelector('.js_clientnumber');
+const inputClue = document.querySelector('.js_clue');
+const inputCounter = document.querySelector('.js_counter');
+const buttonPrueba = document.querySelector('.js_buttonprueba');
+
+//defino valor máximo de getRandomNumber en MaxNumber como constante para que no pase de 100//
+const maxNumber = 100;
+
+//FUNCIONES//
+// dada por el ejercicio, función que saca números random //
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
+// Función guessTheNumber para sacar el número random por la consola y que lo compare al que la usuaria mete en el input//
+function guessTheNumber() 
+{
+    const randomNumber = getRandomNumber(maxNumber);
+    const value = inputNumber.value;
+    console.log(randomNumber)
+}
